29993086462a97948530d933badf4334643d61ec
foefugl <seeddroog@gmail.com>
2021-06-10 15:44:23 +0800

    1. Add command pattern base. 2. Import level design plug-in

---
+++ Assets/Scripts/MoveCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommandPattern.Move
+{
+    public class MoveCommand : Command
+    {
+        private MovementCommander commander;
+
+        public MoveCommand()
+        {
+            Debug.Log("無參建構子");
+        }
+        public MoveCommand(MovementCommander _commander) : this()
+        {
+            Debug.Log("Commander inject");
+            commander = _commander;
+        }
+
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Execute(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/MovementCommander.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommandPattern.Move;
+
+public class MovementCommander : MonoBehaviour
+{
+    private MoveCommand moveCommand;
+
+    private void Start()
+    {
+        moveCommand = new MoveCommand();
+    }
+    public 
+}
